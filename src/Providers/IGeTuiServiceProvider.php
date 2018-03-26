73374a00ed2f3f40ec937de92f5297d7e0aebbd9
zhuangminxue <zhuangminxue@163.com>
2018-03-26 20:12:40 +0800

    import

---
+++ src/Providers/IGeTuiServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/26 0026
+ * Time: 20:03
+ */
+use Illuminate\Support\ServiceProvider;
+
+class IGeTuiServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        parent::boot();
+        //载入个推sdk
+        require_once __DIR__ . '/../../lib/IGt.Push.php';
+        require_once __DIR__ . '/../../lib/igetui/IGt.AppMessage.php';
+        require_once __DIR__ . '/../../lib/igetui/IGt.APNPayload.php';
+        require_once __DIR__ . '/../../lib/igetui/template/IGt.BaseTemplate.php';
+        require_once __DIR__ . '/../../lib/IGt.Batch.php';
+        require_once __DIR__ . '/../../lib/igetui/utils/AppConditions.php';
+    }
+}
